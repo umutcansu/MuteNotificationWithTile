acbec80c9008be24800c466592eb31415a9208e2
Umut Cansu <umutcansugmail.com>
2020-04-14 19:23:38 +0300

    bootreceiver permission for brand  https://en.dev4app.com/archives/44383983-how-to-programmatically-enable-auto-start-and-floating-window-permissions.html

---
+++ app/src/main/java/com/thell/mutenotification/helper/bootreceiver/BootReceiverPrefHelper.kt
@@ -0,0 +1,32 @@
+package com.thell.mutenotification.helper.bootreceiver
+
+import android.content.Context
+
+class BootReceiverPrefHelper
+{
+    companion object
+    {
+
+        private const val FILE_NAME = "AUTOSTART_PREF_FILE"
+        private const val AUTOSTART_KEY = "AUTOSTART_KEY"
+
+        fun writeBoolean(context: Context, state:Boolean)
+        {
+            val sharedPref = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+            val  editor = sharedPref.edit()
+            editor.putBoolean(AUTOSTART_KEY,state)
+            editor.apply()
+
+        }
+
+        fun readBoolean(context: Context):Boolean
+        {
+            val sharedPref = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+
+            return sharedPref.getBoolean(AUTOSTART_KEY,false)
+
+        }
+
+
+    }
+}
